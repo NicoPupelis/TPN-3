c83268fb446b4fde5ca195a7f38cfa8826cc6f60
NicoPupelis <132392870+NicoPupelis@users.noreply.github.com>
2023-06-06 20:29:48 -0300

    Update tpn3ej16.py

--- tpn3ej16.py
+++ tpn3ej16.py
@@ -1,14 +1,29 @@
-Primer documento: Doc 1 - E
-Documento 1: Doc 2 - E
-Documento 2: Doc 3 - E
-Documentos en cola:
-Doc 4 - E
-Doc 5 - E
-Doc 1 - TI
-Doc 2 - TI
-Doc 1 - G
-Doc 2 - G
+import queue
 
+cola = queue.PriorityQueue()
 
-** Process exited - Return Code: 0 **
-Press Enter to exit terminal
+cola.put((1, "Doc 1 - E"))
+cola.put((1, "Doc 2 - E"))
+cola.put((1, "Doc 3 - E"))
+
+primer_doc = cola.get()
+print("Primer documento:", primer_doc[1])
+
+cola.put((2, "Doc 1 - TI"))
+cola.put((2, "Doc 2 - TI"))
+
+cola.put((3, "Doc 1 - G"))
+
+doc_1 = cola.get()
+doc_2 = cola.get()
+print("Documento 1:", doc_1[1])
+print("Documento 2:", doc_2[1])
+
+cola.put((1, "Doc 4 - E"))
+cola.put((1, "Doc 5 - E"))
+cola.put((3, "Doc 2 - G"))
+
+print("Documentos en cola:")
+while not cola.empty():
+    doc = cola.get()
+    print(doc[1])
